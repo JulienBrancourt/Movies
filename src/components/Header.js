4a55fcd0f9a4c37f0c36f0329faf22ef602596b5
ElyanisJ <brancourt.julien@hotmail.fr>
2023-10-13 17:34:31 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/" className={lien => lien.isActive ? "nav-active":""}>Accueil</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/coups-de-coeur" className={lien => lien.isActive ? "nav-active":""}>Coups de coeur</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        
+            <h1>React Movies</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/pages/LikePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const LikePage = () => {
+  return (
+    <div className='user-list-page'>
+      <Header />
+      <h1>LikePage !!</h1>
+    </div>
+  )
+}
+
+export default LikePage;
